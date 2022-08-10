be73fe24a9d29cbbfb6a234aa0c2033e926c5e48
Mahin05 <mahin.islam05@gmail.com>
2022-08-10 20:58:22 +0600

    delete functionalities implemented

--- src/App.js
+++ src/App.js
@@ -1,69 +1,75 @@
-import { addDoc, collection, doc, onSnapshot, query, QuerySnapshot, updateDoc } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, onSnapshot, query, QuerySnapshot, updateDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import './App.css';
 import Todo from './Todo';
 import db from './firebase'
 const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CBFE0]`,
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-cyan-500 to-blue-500`,
   container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
   heading: `text-3xl font-bold text-center  text-gray-800 p-2`,
   form: `flex justify-between`,
   input: `border p-2  w-full text-xl`,
-  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  button: `border p-4 ml-2 bg-blue-500 text-slate-100`,
   count: `text-center p-2 `
 }
 function App() {
   const [todos, setTodos] = useState([]);
-  const [input,setInput] = useState('');
+  const [input, setInput] = useState('');
   console.log(input)
-// create todos
-const createTodo = async (e) => {
-  e.preventDefault(e);
-  if(input === ''){
-    alert('Enter Your TODO');
-    return;
-  }
-  await addDoc(collection(db,'todos'),{
-    text:input,
-    completed:false,
+  // create todos
+  const createTodo = async (e) => {
+    e.preventDefault(e);
+    if (input === '') {
+      alert('Enter Your TODO');
+      return;
+    }
+    await addDoc(collection(db, 'todos'), {
+      text: input,
+      completed: false,
 
-  })
-  setInput('');
-}
+    })
+    setInput('');
+  }
 
   // read todos from firebase
-  useEffect(()=>{
-    const q= query(collection(db,'todos'));
-    const unsubscribe = onSnapshot(q,(querySnapshot)=>{
+  useEffect(() => {
+    const q = query(collection(db, 'todos'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let todosArr = [];
-      querySnapshot.forEach((doc)=>{
-        todosArr.push({...doc.data(),id: doc.id,})
+      querySnapshot.forEach((doc) => {
+        todosArr.push({ ...doc.data(), id: doc.id, })
       });
       setTodos(todosArr)
     });
-    return ()=> unsubscribe()
-  },[])
+    return () => unsubscribe()
+  }, [])
   // Update todo
-   const toggleComplete = async (todo) => {
-    await updateDoc(doc(db,'todos',todo.id),{
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, 'todos', todo.id), {
       completed: !todo.completed
     })
-   }
+  }
+
+  //  delete Todo
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, 'todos', id))
+  }
 
   return (
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>TODO List</h3>
         <form onSubmit={createTodo} className={style.form}>
-          <input value={input} onChange={(e)=>setInput(e.target.value)} className={style.input} type="text" placeholder='Add TODO' />
-          <button className={style.button}><span style={{ 'font-size': '30px' }}>+</span></button>
+          <input value={input} onChange={(e) => setInput(e.target.value)} className={style.input} type="text" placeholder='Add TODO' />
+          <button className={style.button}><span style={{ 'font-size': '20px' }}>+</span></button>
         </form>
-        <ul>
+        {todos.length < 1 ? <p className='text-center mt-5'>Loading...</p> : <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo} toggleComplete={toggleComplete}></Todo>
+            <Todo key={index} todo={todo} toggleComplete={toggleComplete} deleteTodo={deleteTodo}></Todo>
           ))}
-        </ul>
-        {todos.length<1 ? null : <p className={style.count}>{`You have ${todos.length} ToDos`}</p> }
+        </ul>}
+
+        {todos.length < 1 ? null : <p className={style.count}>{`You have ${todos.length} ToDos`}</p>}
       </div>
     </div>
   );
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
 const style = {
-    li:`flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+    li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
     liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
-    row:`flex`,
-    text:`ml-2 cursor-pointer`,
-    textComplete:`ml-2 cursor-pointer line-through`,
-    button:`cursor-pointer flex items-center`,
-    
-}
-const Todo = ({todo,toggleComplete}) => {
+    row: `flex`,
+    text: `ml-2 cursor-pointer`,
+    textComplete: `ml-2 cursor-pointer line-through`,
+    button: `cursor-pointer flex items-center`,
+  };
+const Todo = ({ todo, toggleComplete, deleteTodo }) => {
     return (
         <li className={todo.completed ? style.liComplete : style.li}>
             <div className={style.row}>
-            <input onChange={()=>toggleComplete(todo)} type="checkbox" checked={todo.completed ? 'checked':''} />
-            <p onClick={()=>toggleComplete(todo)} className={todo.completed ? style.textComplete : style.text}>{todo.text}</p>
+                <input onChange={() => toggleComplete(todo)} type="checkbox" checked={todo.completed ? 'checked' : ''} />
+                <p onClick={() => toggleComplete(todo)} className={todo.completed ? style.textComplete : style.text}>{todo.text}</p>
             </div>
-            <button>Delete</button>
+            <button style={{ 'color': 'red', 'font-size': '10px','font-weight':'bold'}} onClick={() => deleteTodo(todo.id)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+            </svg></button>
         </li>
 
     );
