604075ab9201712d0e93ed570b59295508008330
Mahin05 <mahin.islam05@gmail.com>
2022-08-10 19:52:13 +0600

    Fetching functionalities implemented

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import { useState } from 'react';
+import { collection, onSnapshot, query, QuerySnapshot } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Todo from './Todo';
-const style={
-  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CBFE0]`,
-  container:`bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
-  heading:`text-3xl font-bold text-center  text-gray-800 p-2`,
-  form:`flex justify-between`,
-  input:`border p-2  w-full text-xl`,
-  button:`border p-4 ml-2 bg-purple-500 text-slate-100`,
-  count:`text-center p-2 `
+import db from './firebase'
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CBFE0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center  text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2  w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2 `
 }
 function App() {
-  const [todos,setTodos] = useState(['Nipa ke valobashte hobe','Valo thakte hobe']);
+  const [todos, setTodos] = useState([]);
+  // read todos from firebase
+  useEffect(()=>{
+    const q= query(collection(db,'todos'));
+    const unsubscribe = onSnapshot(q,(querySnapshot)=>{
+      let todosArr = [];
+      querySnapshot.forEach((doc)=>{
+        todosArr.push({...doc.data(),id: doc.id,})
+      });
+      setTodos(todosArr)
+    });
+    return ()=> unsubscribe()
+  },[])
+
   return (
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>TODO List</h3>
         <form className={style.form}>
           <input className={style.input} type="text" placeholder='Add TODO' />
-          <button className={style.button}><span style={{'font-size':'30px'}}>+</span></button>
+          <button className={style.button}><span style={{ 'font-size': '30px' }}>+</span></button>
         </form>
         <ul>
-          {todos.map((todo,index)=>(
+          {todos.map((todo, index) => (
             <Todo key={index} todo={todo}></Todo>
           ))}
         </ul>
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEIFYE0mTcNfAa43_dzD1rX77X_wYgLmo",
+  authDomain: "todo-b3c01.firebaseapp.com",
+  projectId: "todo-b3c01",
+  storageBucket: "todo-b3c01.appspot.com",
+  messagingSenderId: "1056624350889",
+  appId: "1:1056624350889:web:f4474a3b0178979111fbb8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export default db;
